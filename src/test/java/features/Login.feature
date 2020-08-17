dc01a8ae33539a3eb2faf24cac5a1b62cf1f0e33
Radwa Saleh <radwa.saleh@pixelogicmedia.com>
2020-08-17 15:40:51 +0200

    1- Add properties fileto save credentials 2- Add a successful login scenario 3- Add Background annotation for common steps

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -1,8 +1,15 @@
 Feature: Login
   This feature deals with the authentication functionality of the application
 
-  Scenario Outline: Login with invalid username and password
+  Background:
     Given user navigates to login page
+
+  Scenario: Successful login with valid username and password
+    When user enters valid credentials
+    And  user clicks login button
+    Then login should be successful
+
+  Scenario Outline: Unsuccessful login with invalid username and password
     When user enters invalid "<username>" and/or "<password>"
     And user clicks login button
     Then login should be unsuccessful
--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -4,17 +4,30 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import dataProvider.ConfigFileReader;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class LoginStep {
     private WebDriver driver = null;
+    private String loginUrl = "https://the-internet.herokuapp.com/login";
+    private ConfigFileReader configFileReader= new ConfigFileReader();
+    private String username = configFileReader.getProperty("username");
+    private String password = configFileReader.getProperty("password");
+    private String successfulLoginMsg = "You logged into a secure area!";
 
     @Given("^user navigates to login page$")
     public void navigateToLoginPage() throws Throwable{
         driver = new ChromeDriver();
-        driver.navigate().to("https://the-internet.herokuapp.com/login");
+        driver.navigate().to(loginUrl);
+    }
+
+    @When("^user enters valid credentials$")
+    public void userEntersValidCredentials( ) throws Throwable {
+        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
     }
 
     @When("^user enters invalid \"([^\"]*)\" and/or \"([^\"]*)\"$")
@@ -30,11 +43,18 @@ public class LoginStep {
 
     @Then("^login should be unsuccessful$")
     public void validateSuccessfulLogin() throws Throwable{
-        if(driver.getCurrentUrl().equalsIgnoreCase("https://the-internet.herokuapp.com/login")){
+        if(driver.getCurrentUrl().equalsIgnoreCase(loginUrl)){
             System.out.println("Test Passed");
         }else {
             System.out.println("Test Failed");
         }
+        driver.close();
+    }
+
+    @Then("^login should be successful$")
+    public void checkSuccessfulLoginMessage(){
+        driver.findElement(By.className("flash success")).isDisplayed();
+        driver.findElement(By.className("flash success")).getText().contains(successfulLoginMsg);
         driver.close();
     }
 }
